072858777cfdba326500f2355311558a3b734079
inverse-d <user@noemail.org>
2025-11-17 19:56:14 +0100

    Switch to dark mode

--- init.lua
+++ init.lua
@@ -888,7 +888,7 @@ require('lazy').setup({
     name = 'catppuccin',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'catppuccin-latte'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 
